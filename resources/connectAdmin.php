5a19821b7cba43204a62c2e94370139af184e93a
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-25 18:16:45 -0700

    fixed connection errors

--- resources/connectAdmin.php
+++ resources/connectAdmin.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "";
-$username = "";
-$password = "";
-$database = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$database);
-
-// Check connection
-if ($conn->connect_error && $_SERVER['HTTP_HOST'] === 'localhost') {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once "connectRaw.php";
 require_once "safePOSTAndGet.php";
