e8dd1d45def168105e85f2441874dbb2615febdd
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-25 18:16:45 -0700

    fixed connection errors

--- resources/connectRaw.php
+++ resources/connectRaw.php
@@ -8,6 +8,6 @@ $database = "";
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
-if ($conn->connect_error && $_SERVER['HTTP_HOST'] === 'localhost') {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    log_error("Connect failed ","",$conn->connect_error);
 }
