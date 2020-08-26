9e9a8dfe5c26623ba7e7a6ee67a3f125c169e03d
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-25 17:48:48 -0700

    set log location

--- resources/logError.php
+++ resources/logError.php
@@ -2,6 +2,7 @@
 function log_error($human_readable,$admin_readable="",$data="") {
     $id=time().hash("sha256",$human_readable.$admin_readable.$data.time());
     $e=new Exception();
+    ini_set('error_log', $_SERVER['DOCUMENT_ROOT']."../error_log.txt");
     error_log("ERROR ".$id.": ".$human_readable." (".$admin_readable.")". $data. " Stack trace: ".$e->getTraceAsString());
     echo("An ERROR has occurred, please tell the website administrator this code: ".$id);
     http_response_code(500);
