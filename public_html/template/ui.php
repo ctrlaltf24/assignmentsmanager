06e7a5576b31ba11fddbd7ee120f9f5e609ca119
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-25 18:20:15 -0700

    removed document root

--- public_html/template/ui.php
+++ public_html/template/ui.php
@@ -83,7 +83,7 @@ function dirToArray($dir) {
  * This is used as it needs to be done twice
  */
 function get_header_items($logged_in=false,$is_teacher=false,$drawer=false){
-    require_once $_SERVER['DOCUMENT_ROOT']."/../resources/startsWithEndsWith.php";
+    require_once "../resources/startsWithEndsWith.php";
     $array=array();
     if($logged_in) {
         if ($is_teacher) {
