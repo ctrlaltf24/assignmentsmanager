6660edc9abe233368492903f6edce57276f25cbb
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-25 17:36:06 -0700

    added stack trace

--- resources/logError.php
+++ resources/logError.php
@@ -1,8 +1,8 @@
 <?php
 function log_error($human_readable,$admin_readable="",$data="") {
     $id=time().hash("sha256",$human_readable.$admin_readable.$data.time());
-    //TODO: add a stacktrace
-    error_log("ERROR ".$id.": ".$human_readable." (".$admin_readable.")". $data);
+    $e=new Exception();
+    error_log("ERROR ".$id.": ".$human_readable." (".$admin_readable.")". $data. " Stack trace: ".$e->getTraceAsString());
     echo("An ERROR has occurred, please tell the website administrator this code: ".$id);
     http_response_code(500);
     exit(500);
