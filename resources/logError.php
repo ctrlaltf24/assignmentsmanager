a5686a9f5cc8a795ba06b5655d0438c3e3f10bbf
Nathan Shaaban <70006231+ctrlaltf24@users.noreply.github.com>
2020-08-27 15:37:25 -0700

    added requested url to log

--- resources/logError.php
+++ resources/logError.php
@@ -3,7 +3,7 @@ ini_set('error_log', $_SERVER['DOCUMENT_ROOT']."/../error_log.txt");
 function log_error($human_readable,$admin_readable="",$data="") {
     $id=time().hash("sha256",$human_readable.$admin_readable.$data.time());
     $e=new Exception();
-    error_log("ERROR ".$id.": ".$human_readable." (".$admin_readable.")". $data. " Stack trace: ".$e->getTraceAsString());
+    error_log("ERROR ".$id.": when visiting ".$_SERVER[REQUEST_URI]." ".$human_readable." (".$admin_readable.")". $data. " Stack trace: ".$e->getTraceAsString());
     echo("An ERROR has occurred, please tell the website administrator this code: ".$id);
     http_response_code(500);
     exit(500);
